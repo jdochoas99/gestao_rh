9f1ba99fd8122d2d092604b70e9f796033405c29
juan.david <juan.david@sefin.ro.gov.br>
2020-12-23 09:39:49 -0400

    tentativa e deu erro

--- apps/empresa/views.py
+++ apps/empresa/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Empresa
 
 
@@ -14,3 +14,8 @@ class EmpresaCreate(CreateView):
         funcionario.empresa = obj
         funcionario.save()
         return HttpResponse('ok')
+
+
+class EmpresaEdit(UpdateView):
+    model = Empresa
+    fields = ['nome']
