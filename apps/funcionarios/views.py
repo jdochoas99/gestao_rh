72b129e3b145377356bd5a7e8b42a82a38b4a920
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2021-01-07 10:33:59 -0400

    19. Crud Funcionarios

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Funcionario
 
 
 # Create your views here.
-def home(request):
-    return HttpResponse('ola')
+
+
+class FuncionariosList(ListView):
+    model = Funcionario
