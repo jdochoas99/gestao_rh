7c29243d8a60af1007d66a8b06a2670042327215
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2021-01-08 08:35:04 -0400

    25. Crud Departamentos, Create

--- apps/departamentos/models.py
+++ apps/departamentos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+
 from apps.empresas.models import Empresa
 
 # Create your models here.
@@ -7,3 +9,6 @@ class Departamento(models.Model):
     empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
     def __str__(self):
         return self.nome
+
+    def get_absolute_url(self):
+        return reverse('list_departamentos')
