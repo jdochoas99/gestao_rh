16d3862e0f6c2d51b3c0af8b6efc5bcbb7be1b8f
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2021-01-13 12:33:25 -0400

    aula 30 - Crud Documentos, Parte 2

--- apps/documentos/urls.py
+++ apps/documentos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from .views import DocumentoCreate
 urlpatterns = [
 
-    path('novo', DocumentoCreate.as_view(), name='create_documento'),
-]
+    path('novo/<int:funcionario_id>/', DocumentoCreate.as_view(), name='create_documento'),
+    ]
