1fa392d9601b360f1deffab8d168099df7c78152
Juan David Ochoa Saavedra <juan.david@sefin.ro.gov.br>
2021-01-14 09:09:39 -0400

    32. Listagem do banco de horas

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from .models import RegistroHoraExtra
 
-# Create your views here.
+
+class HoraExtraList(ListView):
+    model = RegistroHoraExtra
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
