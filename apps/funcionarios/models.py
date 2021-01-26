1b41b6f0ea18943ad780658922893c1c26189ee4
Juan David Ochoa Saavedra <jdochoas99@gmail.com>
2021-01-26 15:37:11 -0400

    Soluçoes basicas

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -4,12 +4,15 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from apps.departamentos.models import Departamento
 from apps.empresas.models import Empresa
+
+
 # Create your models here.
 class Funcionario(models.Model):
     nome = models.CharField(max_length=100)
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     departamentos = models.ManyToManyField(Departamento)
-    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True,blank=True)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
     def __str__(self):
         return self.nome
 
